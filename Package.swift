283d1797a01fcc6fca60a4a26301c9d9149179ae
Stefan Urbanek <stefan@agentfarms.net>
2022-07-15 12:35:42 +0200

    Added forgotten commit for package dependency
    - Added dependency on swift-argument-praser for the flow tool

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
 //        .package(path: "../../Projects/Tarot"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -36,7 +37,10 @@ let package = Package(
             ]),
         .executableTarget(
             name: "FlowTool",
-            dependencies: ["Flows"]),
+            dependencies: [
+                "Flows",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]),
         .testTarget(
             name: "FlowsTests",
             dependencies: ["Flows"]),
